b515999b649b353baf20bace0a97d72c8b0392c6
Katie Lin <katielin019@gmail.com>
2024-02-14 12:54:21 -0500

    add composition implementation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,47 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private IntegerList list;
+    private int totalAdded;
+
+    public DelegationSortedIntList() {
+        list = new SortedIntList();
+        totalAdded = 0;
+    }
+
+    public boolean add(int num) {
+        totalAdded++;
+        return list.add(num);
+    }
+
+    public boolean addAll(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
+        return list.size() > 0;
+    }
+
+
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    public boolean remove(int num) {
+        return list.remove(num);
+    }
+
+    public boolean removeAll(IntegerList list) {
+        return list.removeAll(list);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 
 }
